423a0d47b9e0178088cd8c6aaae46ae283063850
davivieirar <davivr2009@outlook.com>
2023-07-03 21:04:34 -0300

    adicionando metodo get pelo id

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const users = require('./src/data/users.json')
+const express = require('express');
+const app = express();
+const port = 3000;
+const users = require('./src/data/users.json');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Que lindo cara!');
 })
 
 app.get('/users', (req, res) => {
-  res.send(users).json()
+  res.send(users).json();
 })
 
+app.get('/user/:id', (req, res) => {
+    let idParam = parseInt(req.params.id);
+
+    console.log('users', users);
+
+
+    let userFiltered = users.filter(user => user.id === idParam)
+
+    res.status(200).send(userFiltered);
+})
+ 
 app.listen(port, () => {
-  console.log(`Example app listening on port http:://localhost:${port}`)
+  console.log(`Example app listening on port http:://localhost:${port}`);
 })
